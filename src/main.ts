e957e6dc107f5068ec16084cd72851ccaaed26f9
Lewis Wright <lewis@wrightlinedesign.com>
2022-02-26 19:17:09 +0000

    initial project commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css';
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = `
+  <h1>Hello World!</h1>
+  Get Vite setup so we can add the game stuff later
+`;
